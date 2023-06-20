f7a22de71fa228e4ea93df54b1e7bac45d2f67b7
A.H <haron.com>
2023-06-19 19:35:59 -0700

    cc

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,7 +11,12 @@ int main(void)
 	char ch = 'a';
 
 	while (ch <= 'z')
-
+	{
+                if (ch == 'e' || ch == 'q')
+                        ch++;
+                putchar(ch);
+                ch++;
+        }
 	putchar('\n');
 	return (0);
 }
