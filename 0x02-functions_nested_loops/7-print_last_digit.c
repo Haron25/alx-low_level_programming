7f97b5a1d748a701ecd9686a061b9aa1ded3bf36
A.H <haron.com>
2023-06-21 19:04:59 -0700

    z

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,12 @@
 
 int print_last_digit(int n)
 {
-	int lastdigit;
+	int lastDigit;
 
 	if (n < 0)
-		lastdigit = -1 * (n % 10);
+		lastDigit = -1 * (n % 10);
 	else
-		lastdigit = n % 10;
-	putchar(lastdigit + '0');
-	return (lastdigit);
+		lastDigit = n % 10;
+	putchar(lastDigit + '0');
+	return (lastDigit);
 }
